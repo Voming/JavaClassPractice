893203a339345604a8320e1ef0e5c7d876ec9cfe
Voming <ebomin1234@gmail.com>
2024-02-06 18:17:03 +0900

    .

--- JavaClassPractice/src/com/kh/practice2/func/CastingPractice1.java
+++ JavaClassPractice/src/com/kh/practice2/func/CastingPractice1.java
@@ -22,24 +22,24 @@ public class CastingPractice1 {
 		System.out.println("총점 : " + (score1 + score2 + score3));
 		System.out.println("편균 : " + ((score1 + score2 + score3) / 3));
 	}
-	
+
 	public void method3() {
 		int iNum1 = 10;
-		
+
 		float fNum = 3.0f;
-		
+
 		double dNum = 2.5;
-		
+
 		char ch = 'A';
-		
-		System.out.println((int)dNum);
-		System.out.println((double)iNum1);
-		System.out.println(dNum);
-		System.out.println((int)fNum);
-		System.out.println((double)iNum1 / fNum);
-		System.out.println(ch);
-		System.out.println((int)ch);
-		System.out.println((int)ch + iNum1);
-		System.out.println((char)((int)ch + iNum1));
+
+		System.out.println((int) dNum); // 2
+		System.out.println((double) iNum1); // 10.0
+		System.out.println(dNum); // 2.5
+		System.out.println((int) fNum); // 3
+		System.out.println((double) iNum1 / fNum); // 3.3333333333333335
+		System.out.println(ch); // A
+		System.out.println((int) ch); // 65
+		System.out.println((int) ch + iNum1);// 75
+		System.out.println((char) ((int) ch + iNum1));// K
 	}
 }
