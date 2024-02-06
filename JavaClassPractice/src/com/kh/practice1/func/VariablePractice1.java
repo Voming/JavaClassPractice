3b9e11d36288d97123bbd1774de8ff7ef3e4449a
Voming <ebomin1234@gmail.com>
2024-02-06 17:56:55 +0900

    변수 2-1 실습문제

--- JavaClassPractice/src/com/kh/practice1/func/VariablePractice1.java
+++ JavaClassPractice/src/com/kh/practice1/func/VariablePractice1.java
@@ -3,38 +3,50 @@ package com.kh.practice1.func;
 import java.util.Scanner;
 
 public class VariablePractice1 {
-	public int x;
-	Scanner sc = new Scanner(System.in);
-	//메소드를 만들고 run에서 실행할 것
-	public void method1(String name, char sex, int age, float height) {
-		System.out.println("이름을 입력하세요 : " + name);
-		System.out.println("성별을 입력하세요(남/여) : " + sex);
-		System.out.println("나이를 입력하세요 : " + age);
-		System.out.println("키를을 입력하세요(cm) : " + height);
-		System.out.println("키 " + height + "cm인 " + age + "살" + sex + name + "님 반갑습니다^^");
+	public Scanner sc = new Scanner(System.in);
+
+	public void method1() {
+		System.out.print("이름을 입력하세요 : ");
+		String name = sc.nextLine();
+		System.out.print("성별을 입력하세요(남/여) : ");
+		String gender = sc.next();
+		System.out.print("나이를 입력하세요 : ");
+		int age = sc.nextInt();
+		System.out.print("키를 입력하세요(cm) : ");
+		double height = sc.nextDouble();
+
+		System.out.printf("키 %.2fcm인 %d살 %s자 %s님 반갑습니다^^", height, age, gender, name);
 	}
-	
+
 	public void method2() {
-	
-		System.out.print("첫 번째 정수 :");
+		System.out.print("첫번째 변수 : ");
 		int num1 = sc.nextInt();
-		System.out.print("두 번째 정수 :");
+		System.out.print("두번째 변수 : ");
 		int num2 = sc.nextInt();
-		System.out.print("두 번째 정수 :");
-		
+
 		System.out.println("더하기 결과 : " + (num1 + num2));
 		System.out.println("빼기 결과 : " + (num1 - num2));
 		System.out.println("곱하기 결과 : " + (num1 * num2));
-		System.out.println("나누기 몫 결과 : " + (num1 / num2));
+		System.out.println("나누기 몫 결과 : " + (num1 / num2)); // 자동으로 int로 형변한되어 소수점은 보이지 않음
+		System.out.println("나누기 결과 : " + ((double) num1 / num2)); // 소수점이 보임
+	}
+
+	public void method3() {
+		System.out.print("가로 : ");
+		double width = sc.nextDouble();
+		System.out.print("세로 : ");
+		double height = sc.nextDouble();
+
+		System.out.println("면적 : " + (width * height));
+		System.out.println("빼기 결과 : " + (width * 2 + height * 2));
 	}
 	
 	public void method4() {
-		System.out.println("4번 문제 실행중");
-		System.out.println("문자열을 입력하세요");
+		System.out.print("문자열을 입력하세요 : ");
 		String str = sc.nextLine();
-		System.out.println("첫번째 문자 : " + str.charAt(0));
-		System.out.println("두번째 문자 : " + str.charAt(1));
-		System.out.println("세번째 문자 : " + str.charAt(2));
 		
+		System.out.println("첫번째 문자 : "+str.charAt(0));
+		System.out.println("두번째 문자 : "+str.charAt(1));
+		System.out.println("세번째 문자 : "+str.charAt(2));
 	}
 }
--- JavaClassPractice/src/com/kh/practice1/run/Run.java
+++ JavaClassPractice/src/com/kh/practice1/run/Run.java
@@ -1,31 +1,14 @@
 package com.kh.practice1.run;
 
-//import com.kh.practice1.func.VariablePractice1;
+import com.kh.practice1.func.VariablePractice1;
 
 public class Run {
-
 	public static void main(String[] args) {
-		/*String name = "아무개";
-		char sex = '남';
-		int age = 20;
-		float height = 180.5f;
-		new VariablePractice1().method1(name, sex, age, height);   //func 패키지의 함수를 가져와서 실행
+		VariablePractice1 v = new VariablePractice1();
 		
-		VariablePractice1 m1 = new VariablePractice1();
-		m1.method1(name, sex, age, height);
-
-		m1.x = 30;
-		
-		m1.method2();
-		m1.method4();*/
-		int a = 10;
-		int b = ++a;
-		System.out.println(a + ", " + b);
-
-		int a2 = 10;
-		int b2 = a2++;
-		System.out.println(a2 + ", " + b2);
-		System.out.println(a2 + b2);
-	
+		//v.method1();
+		//v.method2();
+		//v.method3();
+		v.method4();
 	}
 }
