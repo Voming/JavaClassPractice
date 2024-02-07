ba6a4c47156d1aaa4cfaaca17a2a47590981b369
Voming <ebomin1234@gmail.com>
2024-02-07 16:33:25 +0900

    스낵 예제

--- JavaClassPractice/src/com/kh/practice/snack/controller/SnackController.java
+++ JavaClassPractice/src/com/kh/practice/snack/controller/SnackController.java
@@ -1,5 +1,25 @@
 package com.kh.practice.snack.controller;
 
-public class SnackController {
+import com.kh.practice.snack.model.vo.Snack;
 
+public class SnackController {
+	Snack s = new Snack();
+
+	public SnackController() {
+	}
+
+	// 데이터를 setter를 이용해 저장하고 저장완료 되었다는 결과를 반환하는 메소드
+	public String saveData(String kind, String name, String flavor, int numOf, int price) {
+		s.setFlavor(flavor);
+		s.setKind(kind);
+		s.setName(name);
+		s.setNumOf(numOf);
+		s.setPrice(price);
+		return "저장 완료 되었습니다.";
+	}
+
+	// 저장된 데이터를 반환하는 메소드
+	public String confirmData() {
+		return s.information();
+	}
 }
--- JavaClassPractice/src/com/kh/practice/snack/view/SnackMenu.java
+++ JavaClassPractice/src/com/kh/practice/snack/view/SnackMenu.java
@@ -1,5 +1,36 @@
 package com.kh.practice.snack.view;
 
-public class SnackMenu {
+import java.util.Scanner;
 
+import com.kh.practice.snack.controller.SnackController;
+
+public class SnackMenu {
+	private Scanner sc = new Scanner(System.in);
+	private SnackController scr = new SnackController();
+
+	// 사용자가 Snack 객체에 데이터를 저장할 수 있도록 값을 받고 저장한값을 출력하는 메소드
+	public void menu() {
+		System.out.println("스낵류를 입력하세요.");
+		System.out.print("종류 : ");
+		String kind = sc.nextLine();
+		System.out.print("이름 : ");
+		String name = sc.nextLine();
+		System.out.print("맛 : ");
+		String flavor = sc.nextLine();
+		System.out.print("개수 : ");
+		String numOf = sc.nextLine();
+		System.out.print("가격 : ");
+		String price = sc.nextLine();
+		
+		int inumOf = Integer.parseInt(numOf);
+		int iprice = Integer.parseInt(price);
+		
+		System.out.println(scr.saveData(kind, name, flavor, inumOf, iprice));
+		System.out.print("저장한 정보를 확인하시겠습니까? (y/n) : ");
+		String info = sc.nextLine();
+		
+		if(info.equals("y")) {
+			System.out.println(scr.confirmData());
+		}
+	}
 }
