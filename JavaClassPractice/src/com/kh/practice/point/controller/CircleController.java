2c924565a46d36e470b3163b82633fe1fe95b745
Voming <68582465+Voming@users.noreply.github.com>
2024-02-11 20:01:38 +0900

    9-1 상속

--- JavaClassPractice/src/com/kh/practice/point/controller/CircleController.java
+++ JavaClassPractice/src/com/kh/practice/point/controller/CircleController.java
@@ -8,12 +8,18 @@ public class CircleController {
 	// 받은 매개변수를 이용하여 필드들을 초기화하고 초기화한 정보와 면적 반환
 	// 면적 : PI * 반지름 * 반지름
 	public String calcArea(int x, int y, int radius) {
+		c.setX(x);
+		c.setY(y);
+		c.setRadius(radius);
 		return "면적 : " + x + ", " + y + ", " + radius + "/ " + (Math.PI * radius * radius);
 	}
 
 	// 받은 매개변수를 이용하여 필드들을 초기화하고 초기화한 정보와 둘레 반환
 	// 둘레 : PI * 반지름 * 2
 	public String calcCircum(int x, int y, int radius) {
+		c.setX(x);
+		c.setY(y);
+		c.setRadius(radius);
 		return "둘레 : " + x + ", " + y + ", " + radius + "/ " + (Math.PI * radius * 2);
 	}
 }
--- JavaClassPractice/src/com/kh/practice/point/controller/RectangleController.java
+++ JavaClassPractice/src/com/kh/practice/point/controller/RectangleController.java
@@ -8,12 +8,20 @@ public class RectangleController {
 	// 받은 매개변수를 이용하여 필드들을 초기화하고 초기화한 정보와 면적 반환
 	// 면적 : 너비 * 높이
 	public String calcArea(int x, int y, int height, int width) {
+		r.setX(x);
+		r.setY(y);
+		r.setHeight(height);
+		r.setWidth(width);
 		return "면적 : " + height + ", " + width + "/ "+ (height * width);
 	}
 
 	// 받은 매개변수를 이용하여 필드들을 초기화하고 초기화한 정보와 둘레 반환
 	// 둘레 : 2 * (너비 + 높이)
 	public String calcPerimeter(int x, int y, int height, int width) {
+		r.setX(x);
+		r.setY(y);
+		r.setHeight(height);
+		r.setWidth(width);
 		return "둘레 : " + height + ", " + width + "/ "+ ((height + width)*2);
 	}
 }
