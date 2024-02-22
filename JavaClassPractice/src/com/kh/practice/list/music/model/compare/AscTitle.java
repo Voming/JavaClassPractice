1da64b557a6148bdf643a399b036ee83977834c7
Voming <ebomin1234@gmail.com>
2024-02-22 14:08:17 +0900

    준비중

--- JavaClassPractice/src/com/kh/practice/list/music/model/compare/AscTitle.java
+++ JavaClassPractice/src/com/kh/practice/list/music/model/compare/AscTitle.java
@@ -2,8 +2,12 @@ package com.kh.practice.list.music.model.compare;
 
 import java.util.Comparator;
 
-public class AscTitle implements Comparator{/*
-public int compare(Object o1, Object o2) {
-// Title의 내림차순으로 정렬하는 기준을 마련한다.
-}*/
+public class AscTitle implements Comparator{
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		// TODO Auto-generated method stub
+		// Title의 내림차순으로 정렬하는 기준을 마련한다.
+		return 0;
+	}
 }
